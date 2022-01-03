09dab584ff2393f63e56d10d1bf1e6b1c01fb173
Felipe Perez <perezmaf@myumanitoba.ca>
2022-01-03 10:23:31 -0500

    Create class to load permafrost dataset for classification

---
+++ codebase/data/satellite.py
@@ -0,0 +1,87 @@
+import pandas as pd
+
+from codebase.utils.transforms import RandomHorizontalFlip, RandomVerticalFlip, ToTensor
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from utils.data_utils import ImageData, normalize
+
+
+class PermafrostDataset(Dataset):
+    train_phase = 'train'
+    test_phase = 'test'
+
+    def __init__(self, data_dir, class_id, is_train=True, transform=None):
+        super(PermafrostDataset).__init__()
+
+        self.data_dir = data_dir
+        self.class_id = class_id
+        self.is_train = is_train
+        self.transform = transform
+
+        if self.is_train:
+            phase = self.train_phase
+        else:
+            phase = self.test_phase
+
+        self.ground_truth = pd.read_csv(f'{data_dir}/{phase}_wkt.csv',
+                                        names=['ImageId', 'ClassType', 'MultipolygonWKT'], skiprows=1)
+        self.grid_sizes = pd.read_csv(f'{data_dir}/grid_sizes.csv',
+                                      names=['ImageId', 'Xmax', 'Ymin', 'Xmin', 'Ymax'], skiprows=1)
+
+        self.images = list(self.ground_truth.ImageId.unique())
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+
+        # TODO Add worker logic for data sharding
+        # https://medium.com/speechmatics/how-to-build-a-streaming-dataloader-with-pytorch-a66dd891d9dd
+
+        # Select image key
+        img_key = self.images[idx]
+
+        # Load image and label
+        image_data = ImageData(self.data_dir, img_key, self.ground_truth, self.grid_sizes)
+        image_data.create_train_feature()
+        image_data.create_label()
+
+        # Retrieve feature and label
+        image, label = image_data.train_feature, image_data.label
+
+        # Apply normalization
+        image = normalize(image)
+
+        # Rescaling
+        image = 2 * image - 1
+
+        # Pick label
+        label = label[self.class_id]
+
+        sample = {'image': image, 'label': label}
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
+
+
+if __name__ == "__main__":
+
+    train_dataset = PermafrostDataset(data_dir='/home/andresf/data/permafrost-imagery/',
+                                      class_id=0,
+                                      is_train=False,
+                                      transform=transforms.Compose([
+                                          ToTensor()
+                                      ]))
+    train_dataloader = DataLoader(train_dataset, batch_size=4)
+
+    for epoch in range(2):
+        print(f"epoch - {epoch}")
+        for batch_idx, sample in enumerate(train_dataloader):
+            images, labels = sample['image'], sample['label']
+            print(f"batch - {batch_idx}")
+            print(images.shape)
+            print(images.sum())
+            print(labels.shape)
+            print(labels.sum())
